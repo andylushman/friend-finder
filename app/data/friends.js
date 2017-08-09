e34350881835f01bcd99122f5afbd629f36930ba
Andy Lushman <andylushman@gmail.com>
2017-08-09 16:11:54 -0600

    Friends data and apiRoutes added

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+//Friends data
+var friendsArray = [
+	{
+		name: "Jon Snow",
+		photo: "http://vignette1.wikia.nocookie.net/gameofthrones/images/1/17/Jon-Snow-Kit-Harington_510.jpeg/revision/latest/scale-to-width-down/2000?cb=20120223002142",
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Cersei Lannister",
+		photo: "https://s-media-cache-ak0.pinimg.com/236x/ce/eb/63/ceeb6316a3168a53dff7c2d514f08bcb--cercei-lannister-queen-cersei.jpg",
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Gregor Clegane',
+		photo: "http://static.tvtropes.org/pmwiki/pub/images/clegane_gregor_stevens_3065.jpg",
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: "Daenerys Targaryen",
+		photo: "http://ll-c.ooyala.com/e1/0zbzl1YjE6aGieXwWfDiAGcsck10zWb9/promo323723448",
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: "Sansa Stark",
+		photo: "https://static.independent.co.uk/s3fs-public/styles/article_medium/public/thumbnails/image/2015/05/18/22/Sansa-Stark-Actress.jpg",
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: "Tyrion Lannister",
+		photo: "http://vignette3.wikia.nocookie.net/gameofthrones/images/c/c3/TyrionEW.png/revision/latest?cb=20120316184837",
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Khal Drogo",
+		photo: "https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F14%2Fc3201ab6-3072-4d3f-b416-ba31736379f0.jpg",
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: "Margaery Tyrell",
+		photo: "http://cdn1us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/natalie_dormer_game_of_thrones.jpg?itok=Kzv-DSj9",
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Jaime Lannister",
+		photo: "http://img1.looper.com/img/gallery/why-game-of-thrones-jaime-lannister-is-more-important-than-we-realized/intro.jpg",
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Ygritte',
+		photo: "https://vignette2.wikia.nocookie.net/gameofthrones/images/2/28/Ygritte-promotionals4pic.jpg/revision/latest/scale-to-width-down/334?cb=20170107042949",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,15 @@
 //required Dependencies
 var path = require("path");
 
-//Default route to Home page
-app.get("/", (app) => {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
-});
+//Export HTML routes
+module.exports = (app) {
+  //Default route to Home page
+  app.get("/", (app) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 
-//Survey Page
-app.get("/survey", (req, res) =>{
-  res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
+  //Survey Page
+  app.get("/survey", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+};
