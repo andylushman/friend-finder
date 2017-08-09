a05f13edc8ae9c1daede0b10babce189b17545d6
Andy Lushman <andylushman@gmail.com>
2017-08-09 13:05:41 -0600

    configured htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,12 @@
-app.get("/", )
+//required Dependencies
+var path = require("path");
 
-app.get("/survey", )
+//Default route to Home page
+app.get("/", (app) => {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+//Survey Page
+app.get("/survey", (req, res) =>{
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
