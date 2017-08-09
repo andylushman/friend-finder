0a623007d5f7a7d07a00c549f5dc300b7445eefd
Andy Lushman <andylushman@gmail.com>
2017-08-09 12:37:49 -0600

    added dependencies

---
+++ server.js
@@ -0,0 +1,19 @@
+/***************
+GLOBAL VARIABLES
+***************/
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+
+/***************
+FUNCTIONS
+***************/
+
+
+
+/***************
+PROCESS
+***************/
