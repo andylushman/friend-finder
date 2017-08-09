5b6afed481e8cc1298719f37be02d77a343f55cc
Andy Lushman <andylushman@gmail.com>
2017-08-09 12:53:37 -0600

    configured express and bodyparser

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,3 @@
+app.get("/", )
+
+app.get("/survey", )
--- server.js
+++ server.js
@@ -6,6 +6,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+//configure express application
+var app = express();
+var PORT = 3000;
+
+//configure bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 
 /***************
@@ -17,3 +27,6 @@ FUNCTIONS
 /***************
 PROCESS
 ***************/
+app.listen(PORT, () => {
+  console.log("Server is listening on PORT: " + PORT);
+});
